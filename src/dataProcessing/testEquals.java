a1d513a302ca550eba5ada0edc909fa68b3496f4
quanminqiche <1444078767@qq.com>
2017-01-12 16:20:58 +0800

    很滑稽、很容易出错的东西

---
+++ src/dataProcessing/testEquals.java
@@ -0,0 +1,13 @@
+package dataProcessing;
+
+public class testEquals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "1";
+		System.out.println("===="+a.equals("1"));
+		System.out.println("===="+a.equals(1));
+		
+	}
+
+}
